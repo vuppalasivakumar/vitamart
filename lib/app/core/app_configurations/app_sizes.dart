9271319e0ef328bbc1aae4a81283fc7a7b67fc34
prapulreddyvita <prapulreddy.vita@gmail.com>
2024-06-20 12:03:05 +0530

    Landing page is done.

--- lib/app/core/app_configurations/app_sizes.dart
+++ lib/app/core/app_configurations/app_sizes.dart
@@ -1,13 +1,17 @@
-import 'package:flutter/material.dart';
+
 
 class AppSizes{
-  static const x0_25=15.0;
-  static const x0_50=25.0;
-  static const x0_75=6.0;
-   static const x1_25=8.0;
-  static const x1_50=10.0;
-  static const x1_75=12.0;
- static const left=EdgeInsets.only(left: x0_25);
- static const horizontal=EdgeInsets.symmetric(horizontal: 15);
-  static const right=EdgeInsets.only(left: x0_50);
+  static const x0_00 = 0.0;
+  static const x0_25 = 2.0;
+  static const x0_50 = 4.0;
+  static const x0_75 = 6.0;
+  static const x1_00 = 8.0;
+  static const x1_25 = 10.0;
+  static const x1_50 = 12.0;
+  static const x1_75 = 14.0;
+  static const x2_00 = 16.0;
+  static const x2_25 = 18.0;
+  static const x2_50 = 20.0;
+  static const x2_75 = 22.0;
+  static const x3_00 = 24.0;
   }
