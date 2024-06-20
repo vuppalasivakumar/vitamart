4acdce2e7c2f4744220112a6a870f873b01f1047
harishvita <challaharish.vita@gmail.com>
2024-06-20 17:34:54 +0530

    account

--- lib/app/core/app_configurations/app_colors.dart
+++ lib/app/core/app_configurations/app_colors.dart
@@ -3,9 +3,6 @@ import 'package:flutter/material.dart';
 class AppColors{
   AppColors._();
   static const primaryColor= Color(0xff1743ef);
-  static const ash= Color(0xffE1E3E1);
-  static const lightblue=Color(0xff17b9ef);
-static const green=Color(0xff08cc22);
-static const balck=Color(0xff000000);
-static const grey=Color(0xffdcdcdc);
+ static const grey=Color.fromARGB(255, 223, 226, 227);
+static const white=Color(0xffffffff);
 } 
--- lib/app/core/app_configurations/app_urls.dart
+++ lib/app/core/app_configurations/app_urls.dart
@@ -1,3 +1,5 @@
 class Appurl{
-  
+  static const accountBannerSponsoredimg="https://tse3.mm.bing.net/th?id=OIP.hhSZ79oQnEPq0vQwaSB2WAHaEK&pid=Api&P=0&h=180";
+  static const shop="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSJNdhF2Ki8vhNQ12YkVGNeb2y3Bug7n4RYgw&s";
+  static const  bajaj="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR-Qx8sTYIlQoU_LMrExpognpgU6JeP766mhg&s";
 }
